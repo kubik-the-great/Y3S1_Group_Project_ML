0871c8a4438a5c48ad14937c5b864c5dd06ec16b
kubik-the-great <146103935+kubik-the-great@users.noreply.github.com>
2024-01-03 16:27:25 +0000

    Final edit to code and datasets
    Cleaned the datasets so they are easy to use
    Can make prediction based on provided data

--- lung_cancer_ml.py
+++ lung_cancer_ml.py
@@ -23,3 +23,9 @@ model.fit(X, y, epochs=150, batch_size=10)
 # evaluate the keras model
 _, accuracy = model.evaluate(X, y)
 print('Accuracy: %.2f' % (accuracy*100))
+
+
+# make probability predictions with the model
+predictions = model.predict(X)
+# round predictions
+rounded = [round(x[0]) for x in predictions]
